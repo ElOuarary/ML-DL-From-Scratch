9e217b4776ca4d51df4f14c40f583f8807e30829
El Ouarary Abdelaziz <abdelazizelouarary@gmail.com>
2026-06-03 14:51:33 +0100

    Defined the Reinforce class for training the agent and using the REINFORCE

--- Reinforcment_Learning/RL_Final_Project/humanoid_setup.py
+++ Reinforcment_Learning/RL_Final_Project/humanoid_setup.py
@@ -1,4 +1,6 @@
+import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 from tensorflow import keras
 from tensorflow.keras import Model
 
@@ -18,4 +20,47 @@ class PolicyNetwork(Model):
     def call(self, state):
         return self.model(state)
     
+class REINFORCE:
+    def __init__(self, obs_space_dim: int, action_space_dim: int):
+        
+        self.learing_rate = .05
+        self.gamma = .99
+        self.policy = PolicyNetwork(obs_space_dim, action_space_dim)
+        self.optimizer = keras.optimizer.Nadam(self.learing_rate)
+        self.tape = tf.GradientTape(persistent=True)
+        
+        self.all_rewards = []
+        self.all_log_prob = []
     
+    def discount_rewards(self, rewards):
+        for i in range(len(rewards) - 2, -1, -1):
+            rewards[i] = self.gamma * rewards[i+1]
+        return rewards
+    
+    def normalize_rewards(self, all_rewards):
+        discoutned_rewards = [self.discount_rewards(rewards) for rewards in all_rewards]
+        flat = np.concatenate(discoutned_rewards)
+        mean, std = flat.mean(), flat.std()
+        return (discoutned_rewards - mean) / (std + 1e-8)
+    
+    def play_step(self, state):
+        mean, std = self.policy(state)
+        distrib = tfp.distributions.Normal(mean, std)
+        actions = distrib.sample()
+        log_prob = tf.reduce_sum(distrib.log_prob(actions), axis=-1)
+        self.all_log_prob.append(log_prob)
+        return actions
+    
+    def update(self):
+        discounted_rewards = self.discount_rewards(self.all_rewards)
+        discounted_rewards_t = tf.constant(discounted_rewards, dtype=tf.float32)
+        
+        log_probs_t = tf.stack(self.all_log_prob)
+        loss = -tf.reduce_mean(discounted_rewards_t * log_probs_t)
+        grads = self.tape.gradient(loss, self.policy.trainable_variables)
+        self.optimizer.apply(zip(grads, self.policy.trainable_variables))
+        self.tape.__exit__()
+        del self.tape
+        self.all_rewards = []
+        self.all_log_prob = []
+        
