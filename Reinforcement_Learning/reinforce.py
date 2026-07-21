7bdbd9a32026887b978e121ea6e659828dcae7ef
ElOuarary <abdlazizelourary@gmail.com>
2026-07-21 21:37:43 +0100

    Fucked up something in the reinforce algorithm, making the problem diverge for some reasons

--- Reinforcement_Learning/reinforce.py
+++ Reinforcement_Learning/reinforce.py
@@ -4,119 +4,113 @@ import tensorflow as tf
 
 from tensorflow import keras
 
-class Agent:
-    def __init__(self, env, net, loss_fn, optimizer, gamma, episodes):
-        self.env = env
-        self.actions = range(2)
-        self.net = net
-        self.loss_fn = loss_fn
-        self.optimizer = optimizer
-        self.gamma = gamma
-        self.max_episodes = episodes
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        proba_distributions = model(obs[np.newaxis])
+        action = np.random.choice([0, 1], p=proba_distributions.numpy()[0])
+        y_target = tf.one_hot([action], depth=2)
+        loss = loss_fn(y_target, proba_distributions)
+    gradient = tape.gradient(loss, model.trainable_variables)
+    obs, reward, terminated, truncated, _ = env.step(action)
+    return obs, reward, terminated, truncated, gradient
 
-    def play_step(self, state):
-        with tf.GradientTape() as tape:
-            proba_distribution = self.net(state)
-            action = np.random.choice(self.actions, p=proba_distribution.numpy()[0])
-            y_target = tf.reshape(tf.one_hot(self.actions, 1), (1, 2))
-            loss = self.loss_fn(y_target, proba_distribution)
-        gradient = tape.gradient(loss, self.net.trainable_variables)
-        return action, gradient
+def play_episode(env, model, loss_fn):
+    obs, _ = env.reset()
+    rewards = []
+    gradients = []
+    while True:
+        obs, reward, terimated, truncated, gradient = play_one_step(env, obs, model, loss_fn)
+        rewards.append(reward)
+        gradients.append(gradient)
+        if terimated or truncated:
+            break
+    return rewards, gradients
 
-    def play_epsisode(self):
-        state, _ = self.env.reset()
-        rewards = []
-        gradients = []
-        while True:
-            action, gradient = self.play_step(state[np.newaxis])
-            next_state, reward, terminated, truncated, _ = self.env.step(action)
-            state = next_state
-            rewards.append(reward)
-            gradients.append(gradient)
-            if terminated or truncated:
-                break
-        return rewards, gradients
-    
-    def discount_reward(self, rewards):
-        rewards = np.array(rewards)
-        for i in range(-1, -len(rewards), -1):
-            rewards[i] = rewards[i] + self.gamma * rewards[i+1]
-        return rewards
-    
-    def baseline_rewards(self, episodes_rewards):
-        discounted_rewards = [self.discount_reward(episode_reward) for episode_reward in episodes_rewards]
-        flat_rewards = np.concatenate(discounted_rewards)
-        mean = flat_rewards.mean()
-        std = flat_rewards.std()
-        return [(discounted_reward - mean) / (std + 1e-8) for discounted_reward in discounted_rewards]
-    
-    def train(self):
-        all_rewards, all_gradients = [], []
-        for _ in range(self.max_episodes):
-            episode_reward, episode_gradient = self.play_epsisode()
-            all_rewards.append(episode_reward)
-            all_gradients.append(episode_gradient)
-        all_discounted_rewards = self.baseline_rewards(all_rewards)
-        scaled_gradient = []
-        for i in range(len(self.net.trainable_variables)):
-            mean_grads = tf.reduce_mean([
-                reward * all_gradients[episode_idx][step_idx][i]
-                for episode_idx, episode in enumerate(all_discounted_rewards)
-                    for step_idx, reward in enumerate(episode)
-            ], axis=0)
-            scaled_gradient.append(mean_grads)
-        self.optimizer.apply_gradients(zip(scaled_gradient, self.net.trainable_variables))
+def play_multiple_episodes(env, model, loss_fn, iterations):
+    all_rewards, all_gradients = [], []
+    for _ in range(iterations):
+        rewards, gradients = play_episode(env, model, loss_fn)
+        all_rewards.append(rewards)
+        all_gradients.append(gradients)
+    return all_rewards, all_gradients
 
-    def test(self, test_env):
-        state, _ = test_env.reset()
-        total_reward = 0
-        while True:
-            proba = self.net(state[np.newaxis]).numpy()[0]
-            action = np.random.choice(self.actions, p=proba)
-            state, reward, terminated, truncated, _ = test_env.step(action)
-            total_reward += reward
-            if terminated or truncated:
-                break
-        return total_reward
+def discount_reward(episode_reward, gamma):
+    episode_reward = np.array(episode_reward)
+    for i in range(len(episode_reward) - 2, -1, -1):
+        episode_reward[i] =  episode_reward[i] + gamma * episode_reward[i+1]
+    return episode_reward
+
+def discounte_normalize(episodes_reward, gamma):
+    discounted_rewards = [discount_reward(episode_reward, gamma) for episode_reward in episodes_reward]
+    flat = np.concatenate(discounted_rewards)
+    mean = flat.mean()
+    std = flat.std()
+    return [(discounted_reward - mean) / (std + 1e-8) for discounted_reward in discounted_rewards]
+
+def test(env, model):
+    obs, _ = env.reset()
+    total_rewards = 0
+    while True:
+        proba_disctribution = model(obs[np.newaxis]).numpy()[0]
+        action = np.random.choice([0, 1], p=proba_disctribution)
+        obs, reward, termianted, truncated, _ = env.step(action)
+        total_rewards += reward
+        if termianted or truncated:
+            break
+    return total_rewards
+
 
 def main():
+    episode_update = 10
+    gamma = 0.95
+
     env = gym.make("CartPole-v1")
     test_env = gym.make("CartPole-v1")
+
     model = keras.Sequential([
-        keras.layers.InputLayer(shape=(4,)),
+        keras.layers.InputLayer((4,)),
         keras.layers.Dense(128),
-        keras.layers.Dense(2,activation="softmax")
+        keras.layers.Dense(2, activation="softmax")
     ])
+
+    optimizer = keras.optimizers.Nadam(learning_rate=0.01)
     loss_fn = keras.losses.BinaryCrossentropy()
-    optimizer = keras.optimizers.Adam()
-    agent = Agent(env, model, loss_fn, optimizer, 0.95, 4)
 
-    iteration = 1
     best_reward = None
+    iteration = 0
     while True:
-        total_rewards = 0
-        agent.train()
+        all_rewards, all_gradients = play_multiple_episodes(env, model, loss_fn, episode_update)
+        all_discounted_normalize_rewards = discounte_normalize(all_rewards, gamma)
 
+        all_mean_grads = []
+        for var_index in range(len(model.trainable_variables)):
+            mean_grads = tf.reduce_mean([
+                reward * all_gradients[eps_idx][step_id][var_index]
+                for eps_idx, episode in enumerate(all_discounted_normalize_rewards)
+                    for step_id, reward in enumerate(episode)
+            ], axis=0)
+            all_mean_grads.append(mean_grads)
+
+        optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+
+        total_reward = 0
         for _ in range(4):
-            episode_reward = agent.test(test_env)
-            if best_reward is None or best_reward < episode_reward:
-                best_reward = episode_reward
-                print(f"Best reward update {best_reward} -> {episode_reward}")
-
-            total_rewards += episode_reward
-            
-        mean_rewards = total_rewards / 4
-        print(f"Iteration {iteration} - Mean Reward: {mean_rewards}")
-
-        if mean_rewards > 195:
+            reward = test(test_env, model)
+            total_reward += reward
+            if best_reward is None or best_reward < reward:
+                print(f"Update best reward {best_reward} -> {reward}")
+                best_reward = reward
+        mean_reward = total_reward / 4
+        print(f"Iteration {iteration} - Mean Reward {mean_reward}")
+        iteration += 1
+        
+        if mean_reward > 490:
             print("Problem Solved")
             break
-        iteration += 1
 
-    demo_env = gym.make("CartPole-v1", render_mode="human")
-    agent.test(demo_env)
-        
-
+    demo = gym.make("CartPole-v1", render_mode="human")
+    demo_test = test(demo, model)
+    print(f"Demo testing Total Reward: {demo_test}")
 
 if __name__ == "__main__":
     main()
