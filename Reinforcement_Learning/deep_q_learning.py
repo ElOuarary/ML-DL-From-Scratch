0fe52e1ba004a50f5ebd36881715bb5f755eaacc
ElOuarary <abdlazizelouarary@gmail.com>
2026-07-27 19:56:12 +0100

    Since DQN did not converge to a solution for the Lunar Landing I did some researchers and changes some parameters and introduced another, along side changing the loss with HuberLoss

--- Reinforcement_Learning/deep_q_learning.py
+++ Reinforcement_Learning/deep_q_learning.py
@@ -1,6 +1,6 @@
 import argparse
 from collections import deque, namedtuple
-from datetime import datetime
+from datetime import UTC, datetime
 
 import gymnasium as gym
 import numpy as np
@@ -8,13 +8,15 @@ import tensorflow as tf
 from tensorflow import keras
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--alpha", type=float, default=0.001)
-parser.add_argument("--batch-size", type=int, default=32)
+parser.add_argument("--alpha", type=float, default=0.0005)
+parser.add_argument("--batch-size", type=int, default=64)
 parser.add_argument("--gamma", type=float, default=0.99)
-parser.add_argument("--buffer-size", type=int, default=1_500)
+parser.add_argument("--buffer-size", type=int, default=10_000)
+parser.add_argument("--test-steps", type=int, default=500)
+parser.add_argument("--netwokr-update", type=int, default=1000)
 
 STEP = namedtuple(
-    "Step", field_names=("state", "action", "reward", "next_state", "done")
+    "Step", field_names=("state", "action", "reward", "next_state", "continue_mask")
 )
 
 
@@ -34,7 +36,7 @@ class Agent:
         self.replay_buffer = deque(maxlen=buffer_size)
 
     def explore(self):
-        if np.random.sample() < max(0.01, self.epsilon):
+        if np.random.sample() < self.epsilon:
             action = self.env.action_space.sample()
         else:
             action = tf.argmax(self.net(self.state[np.newaxis]), axis=-1)
@@ -46,7 +48,7 @@ class Agent:
                 action=action,
                 reward=reward,
                 next_state=next_state,
-                done=0 if terminated or truncated else 1,
+                continue_mask=0 if terminated or truncated else 1,
             )
         )
         if terminated or truncated:
@@ -58,33 +60,29 @@ class Agent:
         indices = np.random.choice(
             range(len(self.replay_buffer)), batch_size, replace=False
         )
-        states, actions, rewards, next_states, done = zip(
+        states, actions, rewards, next_states, continue_mask = zip(
             *[self.replay_buffer[idx] for idx in indices]
         )
-        states, actions, rewards, next_states, done = (
+        states, actions, rewards, next_states, continue_mask = (
             tf.constant(states),
             tf.constant(actions),
             tf.constant(rewards, dtype=tf.float32),
-            tf.constant(next_states, dtype=tf.float32),
-            tf.constant(done, dtype=tf.float32),
+            tf.constant(next_states),
+            tf.constant(continue_mask, dtype=tf.float32),
         )
-        states, rewards, next_states, done = (
-            tf.reshape(states, (batch_size, -1)),
-            tf.reshape(rewards, (batch_size, -1)),
-            tf.reshape(next_states, (batch_size, -1)),
-            tf.reshape(done, (batch_size, -1)),
-        )
-        return states, actions, rewards, next_states, done
+        return states, actions, rewards, next_states, continue_mask
 
     @tf.function
-    def compute_loss(self, state, reward, action, next_state, done):
+    def compute_loss(self, state, reward, action, next_state, continue_mask):
         next_state_value = tf.reduce_max(self.tg_net(next_state), axis=-1)
 
-        q_value_target = reward + self.gamma * done * next_state_value
+        q_value_target = (
+            reward + self.gamma * continue_mask * next_state_value
+        )  # I have a problem here for computin the Bellaman equation
         mask = tf.one_hot(action, self.action_space)
         with tf.GradientTape() as tape:
             q_value = self.net(state)
-            q_value_masked = tf.reduce_sum(mask * q_value, axis=-1, keepdims=True)
+            q_value_masked = tf.reduce_sum(mask * q_value, axis=-1)
             loss = self.loss_fn(q_value_target, q_value_masked)
         gradients = tape.gradient(loss, self.net.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.net.trainable_variables))
@@ -92,8 +90,10 @@ class Agent:
         return loss, tf.reduce_mean(flat_grads)
 
     def train_model(self, batch_size):
-        states, actions, rewards, next_states, done = self.sample_batch(batch_size)
-        loss = self.compute_loss(states, rewards, actions, next_states, done)
+        states, actions, rewards, next_states, continue_mask = self.sample_batch(
+            batch_size
+        )
+        loss = self.compute_loss(states, rewards, actions, next_states, continue_mask)
         return loss
 
     def test(self, test_env):
@@ -123,18 +123,18 @@ def main():
         [
             keras.layers.InputLayer(env.observation_space.shape),
             keras.layers.Dense(256, activation="relu"),
-            keras.layers.Dense(512, activation="relu"),
+            keras.layers.Dense(256, activation="relu"),
             keras.layers.Dense(4),
         ]
     )
     tg_model = keras.models.clone_model(model)
     tg_model.set_weights(model.get_weights())
 
-    loss_fn = keras.losses.MeanSquaredError()
-    optimizer = keras.optimizers.Nadam(learning_rate=alpha)
+    loss_fn = keras.losses.Huber()
+    optimizer = keras.optimizers.Nadam(learning_rate=alpha, clipnorm=1)
     agent = Agent(env, 1, gamma, model, tg_model, loss_fn, optimizer, buffer_size)
 
-    current_time = datetime.now(datetime.timetz).strftime("%Y%m%d-%H%M%S")
+    current_time = datetime.now(UTC).strftime("%Y%m%d-%H%M%S")
     train_logs_dir = "logs/dqn/LunarLander/train/" + current_time
     test_logs_dir = "logs/dqn/LunarLander/test/" + current_time
 
@@ -142,30 +142,31 @@ def main():
     test_summary_writer = tf.summary.create_file_writer(test_logs_dir)
 
     try:
-        for i in range(1, 10_001):
+        for i in range(1, 100_001):
             agent.explore()
-            agent.epsilon = max(1 - i / 9_000, 0.01)
+            agent.epsilon = max(1 - i / 50_000, 0.01)
 
-            if len(agent.replay_buffer) >= 50:
+            if len(agent.replay_buffer) >= 500:
                 loss, gradient_mean = agent.train_model(batch_size)
 
-                total_rewards = 0
-                for _ in range(10):
-                    total_rewards += agent.test(test_env)
+                if i % 500:
+                    total_rewards = 0
+                    for _ in range(10):
+                        total_rewards += agent.test(test_env)
 
-                mean_reward = total_rewards / 10
-                with train_summary_writer.as_default():
-                    tf.summary.scalar("train_loss", loss, step=i)
-                    tf.summary.scalar("gradient_mean", gradient_mean, step=i)
+                    mean_reward = total_rewards / 10
+                    with train_summary_writer.as_default():
+                        tf.summary.scalar("train_loss", loss, step=i)
+                        tf.summary.scalar("gradient_mean", gradient_mean, step=i)
 
-                with test_summary_writer.as_default():
-                    tf.summary.scalar("test_mean_reward", mean_reward, step=i)
+                    with test_summary_writer.as_default():
+                        tf.summary.scalar("test_mean_reward", mean_reward, step=i)
 
-                if mean_reward > 200:
-                    print("Problem Solved")
-                    break
+                    if mean_reward > 200:
+                        print("Problem Solved")
+                        break
 
-            if i % 100 == 0:
+            if i % 1000 == 0:
                 print("Target Model Updating")
                 agent.tg_net.set_weights(agent.net.get_weights())
 
