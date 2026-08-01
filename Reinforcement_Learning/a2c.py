b73cf3cc7e4922d3125c86477f2f4e32229a3a20
ElOuarary <abdlazizelouarary@gmail.com>
2026-08-01 15:27:56 +0100

    went with some modification and solving some issues, getting to understand what is the difference between graph and eager execution

--- Reinforcement_Learning/a2c.py
+++ Reinforcement_Learning/a2c.py
@@ -6,11 +6,14 @@ from tensorflow import keras
 
 gym.register_envs(ale_py)
 
+env = gym.make("ALE/BattleZone-v5")
+
 class Actor2Critic(keras.Model):
     def __init__(self, observation_space, action_space):
         super().__init__()
         self.shared_network = keras.models.Sequential([
-            keras.layers.Conv2D(32, kernel_size=8, strides=4,activation="relu", input_shape=observation_space),
+            keras.layers.InputLayer(observation_space),
+            keras.layers.Conv2D(32, kernel_size=8, strides=4,activation="relu"),
             keras.layers.MaxPool2D(),
             keras.layers.Conv2D(64, kernel_size=4, strides=2, activation="relu"),
             keras.layers.MaxPool2D(),
@@ -27,57 +30,58 @@ class Actor2Critic(keras.Model):
         x = self.shared_network(obs)
         return self.actor(x), self.critic(x)
 
-def env_step(env, obs):
-    next_obs, reward, termiated, truncated, _ = env.step(obs)
+@tf.numpy_function(Tout=[tf.float32, tf.int32, tf.int32])
+def env_step(action):
+    next_obs, reward, termiated, truncated, _ = env.step(action)
     return next_obs, reward, termiated | truncated
 
-def play_episode(env, model):
-    obs, _ = env.reset()
-    actions = []
-    actions_logits = []
-    values = []
-    rewards = []
-    while True:
+@tf.function
+def play_episode(initial_state, model):
+    obs = tf.cast(initial_state, tf.float32)
+    actions_probas = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    values = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    rewards = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    i = 0
+    while tf.constant(True):
         action_logits, state_value = model(obs[np.newaxis])
-        action = tf.random.categorical(actions_logits, 1)[0, 0].numpy()
-        obs, reward, terminated, truncated, _ = env.step(action)
+        action = tf.random.categorical(action_logits, 1)[0, 0]
+        action_proba = tf.nn.softmax(action_logits)
+        obs, reward, done= env_step(action)
 
-        actions.append(action)
-        actions_logits.append(action_logits)
-        values.append(state_value)
-        rewards.append(reward)
-        if terminated or truncated:
+        actions_probas = actions_probas.write(i, action_proba[0, action])
+        values = values.write(i, tf.squeeze(state_value))
+        rewards = rewards.write(i, reward)
+        if tf.cast(done, tf.bool):
             break
+        i += 1
 
-    return actions, actions_logits, values, rewards
+    return actions_probas.stack(), values.stack(), rewards.stack()
 
 def discount_reward(rewards, gamma):
     discounted_reward = np.array(rewards)
     for i in range(len(rewards) - 2, -1, -1):
-        rewards[i] += gamma * rewards[i+1]
+        discounted_reward[i] += gamma * discounted_reward[i+1]
     return (discounted_reward - discounted_reward.mean()) / (discounted_reward.std() - 1e-8)
 
 def play_test_episode(test_env, model):
     states, _ = test_env.reset()
-    actives = np.ones(test_env.num_envs)
-    total_reward = np.zeros(test_env.num_envs)
-    while np.any(actives):
+    while True:
         action_logits, _ = model(states)
         action_probas = tf.nn.softmax(action_logits)
         optimal_action = tf.argmax(action_probas, axis=-1).numpy()[0]
         states, reward, terminated, truncated, _ = test_env.step(optimal_action)
         total_reward += reward
-        actives = np.logical_and(actives, np.logical_not(termiated | truncated))
-    return total_reward.mean()
+        if terminated or truncated:
+            break
+    return total_reward
 
 def main():
     env = gym.make("ALE/BattleZone-v5")
-    test_env = gym.make_vec("ALE/BattleZone-v5", 5)
+    test_env = gym.make("ALE/BattleZone-v5")
     model = Actor2Critic(env.observation_space.shape, 18)
     gamma = 0.99
     reward_threhold = 3000
 
-    actor_loss_fn = keras.losses.CategoricalCrossentropy()
     critic_loss_fn = keras.losses.Huber()
     optimizer = keras.optimizers.Nadam()
 
@@ -85,15 +89,18 @@ def main():
 
     while True:
         with tf.GradientTape() as tape:
-            actions, actions_logits, values, rewards = play_episode(env, model)
-            actions = tf.one_hot(actions, 18)
-            actions_probas = tf.nn.softmax(actions_logits)
-            actor_loss = actor_loss_fn(actions, actions_probas)
+            initial_state, _ = env.reset()
+            actions_probas, values, rewards = play_episode(initial_state, model)
+            actor_loss = - tf.reduce_mean((rewards - values) * tf.math.log(actions_probas))
             discounted_rewards = discount_reward(rewards, gamma)
             critic_loss = critic_loss_fn(discounted_rewards, values)
             loss = actor_loss + critic_loss
+
+        print("Loss", loss)
         grad = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply(zip(grad, model.trainable_variables))
+        print("Grad", grad)
+        optimizer.apply_gradients(zip(grad, model.trainable_variables))
+
 
         if iteration % 1000:
             test_reward = play_test_episode(test_env, model)
@@ -102,6 +109,6 @@ def main():
                 print("Problem Solved!")
 
         iteration += 1
-        
+
 if __name__ == "__main__":
     main()
