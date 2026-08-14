ecdafde47dd86c5c1a0a33dd335d9cc8b719d211
ElOuarary <abdlazizelouarary@gmail.com>
2026-08-14 19:35:14 +0100

    Performed some additional configurations by enabling the programme to be runned from the command line with arguments

--- Reinforcement_Learning/a2c.py
+++ Reinforcement_Learning/a2c.py
@@ -1,69 +1,72 @@
-import gymnasium as gym
-from gymnasium.vector import SyncVectorEnv
+import argparse
+import gc
+from collections import deque
+from datetime import datetime
+from time import perf_counter
+
 import ale_py
+import gymnasium as gym
+import imageio
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
 import tqdm
+from gymnasium.vector import SyncVectorEnv
+from tensorflow import keras
 
-import imageio
-from datetime import datetime
-import gc
-import psutil, os, csv
-from collections import deque
-from time import perf_counter
-from typing import Tuple
+from utils import MetricLog, make_atari_env
 
-from utils import make_atari_env
 
-class MetricLog:
-    def __init__(self, path="metrics.csv"):
-        self.path = path
-        self.process = psutil.Process(os.getpid())
-        with open(self.path, "w") as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                "iteration", "step_time_s", "rss_mb", "cpu_percent"
-            ])
+def build_arg_parser():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--num-envs", default=8, type=int)
+    arg_parser.add_argument("--gamma", default=0.99, type=float)
+    arg_parser.add_argument("--alpha", default=0.001, type=float)
+    arg_parser.add_argument("--entropy-beta", default=0.01, type=float)
+    arg_parser.add_argument("--num-steps", default=5, type=int)
+    arg_parser.add_argument("--reward-threshold", default=5_000, type=float)
+    return arg_parser
 
-    def log(self, iteration,step_time_s):
-        rss = self.process.memory_info().rss / (1024 * 1024)
-        cpu = self.process.cpu_percent()
-        with open(self.path, "a") as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                iteration, step_time_s, rss, cpu
-            ])
 
 class Actor2Critic(keras.Model):
     def __init__(self, observation_space: int, action_space: int):
         super().__init__()
-        self.shared_network = keras.models.Sequential([
-            keras.layers.InputLayer(observation_space),
-            keras.layers.Conv2D(32, kernel_size=8, strides=4,activation="relu"),
-            keras.layers.Conv2D(64, kernel_size=4, strides=2, activation="relu"),
-            keras.layers.Conv2D(64, kernel_size=3, strides=1, activation="relu"),
-            keras.layers.Flatten(),
-            keras.layers.Dense(256, activation="relu"),
-            keras.layers.Dense(256, activation="relu")
-        ])
+        self.shared_network = keras.models.Sequential(
+            [
+                keras.layers.InputLayer(observation_space),
+                keras.layers.Conv2D(32, kernel_size=8, strides=4, activation="relu"),
+                keras.layers.Conv2D(64, kernel_size=4, strides=2, activation="relu"),
+                keras.layers.Conv2D(64, kernel_size=3, strides=1, activation="relu"),
+                keras.layers.Flatten(),
+                keras.layers.Dense(512, activation="relu"),
+            ]
+        )
         self.actor = keras.layers.Dense(action_space)
         self.critic = keras.layers.Dense(1)
 
-    def call(self, obs: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    def call(self, obs: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
         x = self.shared_network(obs)
         return self.actor(x), self.critic(x)
 
+
 class Agent:
-    def __init__(self, env, test_env, model, optimizer, critic_loss_fn, gamma, n_steps=5):
+    def __init__(
+        self, env, test_env, model, optimizer, critic_loss_fn, gamma, beta, n_steps
+    ):
         self.env = env
         self.current_states, _ = env.reset()
+        self.obs_shape = (
+            self.env.num_envs,
+            self.env.observation_space.shape[2],
+            self.env.observation_space.shape[3],
+            self.env.observation_space.shape[1],
+        )
         self.test_env = test_env
         self.model = model
         self.critic_loss_fn = critic_loss_fn
         self.optimizer = optimizer
         self.gamma = gamma
-        self.n_steps = n_steps  
+        self.beta = beta
+        self.n_steps = n_steps
 
     @tf.function
     def predict(self, state):
@@ -71,14 +74,22 @@ class Agent:
         action = tf.random.categorical(action_logits, num_samples=1, dtype=tf.int32)
         return action
 
-    def collect_rollout(self, states: np.ndarray) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-        train_states = np.zeros(shape=(self.n_steps, 8, 84, 84, 4), dtype=np.float32)
-        train_actions = np.zeros(shape=(self.n_steps, self.env.num_envs), dtype=np.int32)
-        train_rewards = np.zeros(shape=(self.n_steps, self.env.num_envs), dtype=np.float32)
+    def collect_rollout(
+        self, states: np.ndarray
+    ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        train_states = np.zeros(shape=(self.n_steps, *self.obs_shape), dtype=np.float32)
+        train_actions = np.zeros(
+            shape=(self.n_steps, self.env.num_envs), dtype=np.int32
+        )
+        train_rewards = np.zeros(
+            shape=(self.n_steps, self.env.num_envs), dtype=np.float32
+        )
         train_dones = np.zeros(shape=(self.n_steps, self.env.num_envs), dtype=np.bool)
 
         for step in range(self.n_steps):
-            states_transpose = np.transpose(states.astype(np.float32), axes=[0, 2, 3, 1]) / 255.0
+            states_transpose = (
+                np.transpose(states.astype(np.float32), axes=[0, 2, 3, 1]) / 255.0
+            )
             actions = self.predict(states_transpose).numpy().flatten()
             next_states, rewards, terminated, _, _ = self.env.step(actions)
 
@@ -91,43 +102,83 @@ class Agent:
 
         self.current_states = states
 
-        next_states = np.transpose(next_states.astype(np.float32), axes=(0, 2, 3, 1)) / 255.0
+        next_states = (
+            np.transpose(next_states.astype(np.float32), axes=(0, 2, 3, 1)) / 255.0
+        )
         _, next_state_value = self.model(next_states)
         boostrapped_values = next_state_value[:, 0].numpy()
-        
+
         train_returns = np.zeros_like(train_rewards)
         for i in reversed(range(self.n_steps)):
-            boostrapped_values = np.where(train_dones[i], train_rewards[i], train_rewards[i] + self.gamma * boostrapped_values)
+            boostrapped_values = np.where(
+                train_dones[i],
+                train_rewards[i],
+                train_rewards[i] + self.gamma * boostrapped_values,
+            )
             train_returns[i] = boostrapped_values
 
         return (
             tf.convert_to_tensor(train_states.reshape(-1, 84, 84, 4), dtype=tf.float32),
-            tf.convert_to_tensor(train_actions.reshape(-1,), dtype=tf.int32),
-            tf.convert_to_tensor(train_returns.reshape(-1,), dtype=tf.float32)
+            tf.convert_to_tensor(
+                train_actions.reshape(
+                    -1,
+                ),
+                dtype=tf.int32,
+            ),
+            tf.convert_to_tensor(
+                train_returns.reshape(
+                    -1,
+                ),
+                dtype=tf.float32,
+            ),
         )
 
-    def compute_loss(self, action_logits: tf.Tensor, action_log_probas: tf.Tensor, state_values: tf.Tensor, returns: tf.Tensor) -> tf.Tensor:
+    def compute_loss(
+        self,
+        action_logits: tf.Tensor,
+        action_log_probas: tf.Tensor,
+        state_values: tf.Tensor,
+        returns: tf.Tensor,
+    ) -> tf.Tensor:
         advantage = tf.stop_gradient(returns - state_values)
-        normalized_advantage = (advantage - tf.reduce_mean(advantage)) / (tf.math.reduce_std(advantage) + 1e-8)
-        action_loss = - tf.reduce_mean(normalized_advantage * action_log_probas)
+        normalized_advantage = (advantage - tf.reduce_mean(advantage)) / (
+            tf.math.reduce_std(advantage) + 1e-8
+        )
+        action_loss = -tf.reduce_mean(normalized_advantage * action_log_probas)
         value_loss = self.critic_loss_fn(returns, state_values)
 
         policy = tf.nn.softmax(action_logits)
         log_policy = tf.nn.log_softmax(action_logits)
         entropy = tf.reduce_mean(tf.math.reduce_sum(policy * log_policy, axis=-1))
 
-        return action_loss + value_loss + 0.01 * entropy, action_loss, value_loss, entropy
+        return (
+            action_loss + value_loss + self.beta * entropy,
+            action_loss,
+            value_loss,
+            entropy,
+        )
 
     @tf.function(
-        input_signature=[tf.TensorSpec(shape=(None, 84, 84, 4), dtype=tf.float32),tf.TensorSpec(shape=(None,), dtype=tf.int32),tf.TensorSpec(shape=(None,), dtype=tf.float32)],
-        jit_compile=True
+        input_signature=[
+            tf.TensorSpec(shape=(None, 84, 84, 4), dtype=tf.float32),
+            tf.TensorSpec(shape=(None,), dtype=tf.int32),
+            tf.TensorSpec(shape=(None,), dtype=tf.float32),
+        ],
+        jit_compile=True,
     )
-    def train_step(self, states: tf.Tensor, actions: tf.Tensor, returns: tf.Tensor):
+    def train_step(
+        self, states: tf.Tensor, actions: tf.Tensor, returns: tf.Tensor
+    ) -> tuple(tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor):
         returns = tf.expand_dims(returns, axis=1)
         with tf.GradientTape() as tape:
             action_logits, state_values = self.model(states)
-            action_log_probas = tf.expand_dims(tf.gather(tf.nn.log_softmax(action_logits), actions, batch_dims=1), axis=1)
-            loss, actor_loss, critic_loss, entropy_loss = self.compute_loss(action_logits, action_log_probas, state_values, returns)
+            action_log_probas = tf.expand_dims(
+                tf.gather(tf.nn.log_softmax(action_logits), actions, batch_dims=1),
+                axis=1,
+            )
+            loss, actor_loss, critic_loss, entropy_loss = self.compute_loss(
+                action_logits, action_log_probas, state_values, returns
+            )
 
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
@@ -135,11 +186,24 @@ class Agent:
 
     def learn_from_episode(self):
         start = perf_counter()
-        current_states, _ = self.env.reset() if self.current_states is None else (self.current_states, None)
+        current_states, _ = (
+            self.env.reset()
+            if self.current_states is None
+            else (self.current_states, None)
+        )
         states, actions, returns = self.collect_rollout(current_states)
-        loss, actor_loss, critic_loss, entropy_loss = self.train_step(states, actions, returns)
+        loss, actor_loss, critic_loss, entropy_loss = self.train_step(
+            states, actions, returns
+        )
         end = perf_counter()
-        return np.mean(returns), loss, actor_loss, critic_loss, entropy_loss, end - start
+        return (
+            np.mean(returns),
+            loss,
+            actor_loss,
+            critic_loss,
+            entropy_loss,
+            end - start,
+        )
 
     def test(self):
         states, _ = self.test_env.reset()
@@ -152,27 +216,40 @@ class Agent:
             states = states.astype(np.float32)[np.newaxis] / 255.0
             action_logits, _ = self.model(states)
             optimal_action = tf.argmax(action_logits, axis=-1).numpy()[0]
-            states, reward, terminated, truncated, _ = self.test_env.step(optimal_action)
+            states, reward, terminated, truncated, _ = self.test_env.step(
+                optimal_action
+            )
             total_reward += reward
             if terminated or truncated:
                 break
         return total_reward, frames
-        
+
+
 def main():
     metrics = MetricLog()
     gym.register_envs(ale_py)
-    envs = SyncVectorEnv([make_atari_env("ALE/BattleZone-v5") for _ in range(8)])
+
+    args = build_arg_parser().parse_args()
+    NUM_ENVS = args.num_envs
+    GAMMA = args.gamma
+    ALPHA = args.alpha
+    ENTROPY_BETA = args.entropy_beta
+    NUM_STEPS = args.num_steps
+    REWARD_THRESHOLD = args.reward_threshold
+
+    envs = SyncVectorEnv([make_atari_env("ALE/BattleZone-v5") for _ in range(NUM_ENVS)])
     test_env = make_atari_env("ALE/BattleZone-v5", render_mode="rgb_array")()
 
     model = Actor2Critic((84, 84, 4), 18)
     dummy_input = tf.zeros((1, 84, 84, 4))
     _ = model(dummy_input)
-    gamma = 0.99
-    critic_loss_fn = keras.losses.Huber()
-    optimizer = keras.optimizers.Nadam(learning_rate=0.0015, clipnorm=0.1)
 
-    agent = Agent(envs, test_env, model, optimizer, critic_loss_fn, gamma, 20)
-    reward_threhold = 3000
+    critic_loss_fn = keras.losses.Huber()
+    optimizer = keras.optimizers.Nadam(learning_rate=ALPHA, clipnorm=0.1)
+
+    agent = Agent(
+        envs, test_env, model, optimizer, critic_loss_fn, GAMMA, ENTROPY_BETA, NUM_STEPS
+    )
 
     current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     train_logs_dir = "logs/A2C/BattleZone/train" + current_time
@@ -180,14 +257,24 @@ def main():
 
     train_summary_writer = tf.summary.create_file_writer(train_logs_dir)
     test_summary_writer = tf.summary.create_file_writer(test_logs_dir)
-    
-    moving_average_reward: deque = deque(maxlen=100)
+
+    moving_average_reward: deque = deque(maxlen=500)
     try:
         t = tqdm.trange(100_000)
         for iteration in t:
-            average_reward, loss, actor_loss, critic_loss, entropy_loss, iteration_time = agent.learn_from_episode()
+            (
+                average_reward,
+                loss,
+                actor_loss,
+                critic_loss,
+                entropy_loss,
+                iteration_time,
+            ) = agent.learn_from_episode()
             moving_average_reward.append(average_reward)
-            t.set_postfix(episode_reward=average_reward, running_reward=np.mean(moving_average_reward))
+            t.set_postfix(
+                episode_reward=average_reward,
+                running_reward=np.mean(moving_average_reward),
+            )
 
             if iteration % 1000 == 0:
                 test_reward, frames = agent.test()
@@ -204,19 +291,20 @@ def main():
                     tf.summary.scalar("entropy loss", entropy_loss, step=iteration)
                     tf.summary.scalar("loss", loss, step=iteration)
                     tf.summary.scalar("iteration_time", iteration_time, step=iteration)
-                
-            
+
                 with test_summary_writer.as_default():
                     tf.summary.scalar("test reward", test_reward, step=iteration)
 
-                if test_reward > reward_threhold:
-                        print("Problem Solved!")
-            
+                if test_reward > REWARD_THRESHOLD:
+                    print("Problem Solved!")
+
     except KeyboardInterrupt:
         pass
     finally:
         envs.close()
         test_env.close()
+        model.save("a2c.keras")
+
 
 if __name__ == "__main__":
     main()
--- Reinforcement_Learning/utils.py
+++ Reinforcement_Learning/utils.py
@@ -1,6 +1,29 @@
 import gymnasium as gym
 from gymnasium.wrappers import AtariPreprocessing, FrameStackObservation
 
+import csv
+import os
+import psutil
+
+class MetricLog:
+    def __init__(self, path="metrics.csv"):
+        self.path = path
+        self.process = psutil.Process(os.getpid())
+        with open(self.path, "w") as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                "iteration", "step_time_s", "rss_mb", "cpu_percent"
+            ])
+
+    def log(self, iteration,step_time_s):
+        rss = self.process.memory_info().rss / (1024 * 1024)
+        cpu = self.process.cpu_percent()
+        with open(self.path, "a") as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                iteration, step_time_s, rss, cpu
+            ])
+
 def make_atari_env(env_id, render_mode=None, grayscale_obs=True):
     def _make():
         env = gym.make(env_id, render_mode=render_mode)
